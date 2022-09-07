4d6ca29e415da104d937e958ec8820f63d86d1a2
Adriane0291 <adrianbloch01@gmail.com>
2022-09-07 12:30:56 +0200

    Initial commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,70 @@
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "family_name")
+    private String familyName;
+    @Column
+    private LocalDate birthday;
+    @Column
+    private String email;
+
+
+    public Employee(String firstName, String familyName, LocalDate birthday, String email) {
+        this.firstName = firstName;
+        this.familyName = familyName;
+        this.birthday = birthday;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Employee() {
+    }
+}
